da25ae1d297e93349d10a883b3a317edb92f0b84
Hamad-Aloqayli <hamad.bader.1997@gmail.com>
2019-09-25 15:53:21 +0300

    new version

--- docToTry.php
+++ docToTry.php
@@ -1,3 +1,17 @@
+<?php 
+
+if(isset($_GET['submit']))
+{
+    if(!empty($_GET['location']))
+    {
+        
+    }
+}
+ 
+    
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,49 +23,12 @@
 <body>
     
 
-<form action="docToTry2.php" method="post" enctype="multipart/form-data">
+<form action="" method="" enctype="">
 
-<input type="file" name="file" >
+<input type="text" name="location">
 <input type="submit" name="submit">
 
 </form>
-
-<?php 
-
-
-// $url = "www.hamad.com";
-// $bom = explode(".",$url);
-// echo end($bom);
-
-$mailTo = "xakowi@mail-desk.net";
-$subject = "Test mail function";
-$message = "Hi\n\nThis message from php";
-$header = "From: test@mail-desk.net";
-
-mail($mailTo,$subject,$message,$header);
-
-
-
-   // $array = [
-   //    ["hamad",21],
-   //    ["fahad",17],
-   //    ["yazeed",10]
-   // ];
-
-   // foreach($array as $key => $value)
-   // {
-   //          echo $value[0] ." and ". $value[1] ."<br>";
-   //          $_REQUEST[];
-   //          $_SERVER[];
-   // }
-    
-
-?>
-
-
-<video src="https://www.youtube.com/watch?v=vNAWpFTuv1s"></video>
-
-
 
 
 <script>
